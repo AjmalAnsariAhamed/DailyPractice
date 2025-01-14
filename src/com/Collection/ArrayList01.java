fbac442d9c6c48dcc8bb07039a80f9d4c3e9f89f
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-14 15:15:43 +0530

    Arrays.asList and List.of example

--- src/com/Collection/ArrayList01.java
+++ src/com/Collection/ArrayList01.java
@@ -1,22 +1,29 @@
 package com.Collection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrayList01 {
     public static void main(String args[]){
-        ArrayList<Integer> list=new ArrayList<>();
-        List<Integer> list1=new ArrayList<>();
-        list1.add(5);
-        list1.add(7);
-        // 5,7
-        list.add(7);
-        list.add(5);
-        //7,5
-        list1.add(1,6);
-        //5,6,7
-        list.set(1,6);
-        //7,6 it replaces
+        // asList returns implementation of List called ArrayList which is inside of
+        // Arrays class java.util.Arrays$ArrayList not java.util.ArrayList
+        //java.util.Arrays$ArrayList != java.util.ArrayList
+        List<Integer> list = Arrays.asList(1,2,3,4);
+        ArrayList<Integer> arraysList=Arrays.asList(1,2,3);//error
+        //no add and remove only set will work
+
+        //List.of
+        List<Integer>list1= List.of(1,2,3);
+        ArrayList<Integer> listOfList=List.of(1,2,3);//error
+        // cant add, set and remove
+
+        // the bestway
+        List<Integer> egList=Arrays.asList(1,2,3);
+        ArrayList<Integer> egArrList = new ArrayList<>(egList);
+        // or in simple
+        ArrayList<Integer> egArrList1=new ArrayList<>(Arrays.asList(1,2,3,4));
+
 
 
     }
