e70545e5cdc4d421cefbd01aa762672595d2e1be
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-14 14:57:59 +0530

    ArrayList creation

---
+++ src/com/Collection/ArrayList01.java
@@ -0,0 +1,11 @@
+package com.Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList01 {
+    public static void main(String args[]){
+        ArrayList<Integer> list=new ArrayList<>();
+        List<Integer> list1=new ArrayList<>();
+    }
+}
