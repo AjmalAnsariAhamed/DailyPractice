1540398fd1a870a185979326289780b5a755c881
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-14 15:02:34 +0530

    ArrayList add and set

--- src/com/Collection/ArrayList01.java
+++ src/com/Collection/ArrayList01.java
@@ -7,5 +7,17 @@ public class ArrayList01 {
     public static void main(String args[]){
         ArrayList<Integer> list=new ArrayList<>();
         List<Integer> list1=new ArrayList<>();
+        list1.add(5);
+        list1.add(7);
+        // 5,7
+        list.add(7);
+        list.add(5);
+        //7,5
+        list1.add(1,6);
+        //5,6,7
+        list.set(1,6);
+        //7,6 it replaces
+
+
     }
 }
