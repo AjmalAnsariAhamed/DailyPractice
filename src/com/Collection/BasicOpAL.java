a24072040f819c151a6bb12a96744b847090cb50
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-15 08:27:47 +0530

    ArrayListOperations-modifying while iterating: its shall not be done through loops , shall use iterator for it read comments

--- src/com/Collection/BasicOpAL.java
+++ src/com/Collection/BasicOpAL.java
@@ -1,6 +1,7 @@
 package com.Collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class BasicOpAL {
@@ -12,15 +13,19 @@ public class BasicOpAL {
    }
    System.out.println(list1);
 
-// this wont work because it removing elements but at the same time elements are also shifting towards// left
+// this won't work because it is removing elements while  iterating elements, also shifting  elements towards left
+// 1,3,8,4
+// lets say first it removes 1 , next it shall remove 3 but 3 will be shifted towards left after removal 1 but iterator
+// will be at second element which is now 8, 3 will be escaped
 //   for(Integer i:list1){
 //       if(i%2!=0){
 //           list1.remove(i);
 //       }
 //   }
+        System.out.println(Collections.max(list1));
+        System.out.println(Collections.min(list1));
         list1.removeIf(x-> (x%2)!=0);
 
         System.out.println(list1);
-
     }
 }
