4fe23c029390cb227a264b7b3bfae9a23c4e999a
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-21 11:03:36 +0530

    FrequentCounter--using--HashMap in FrequencyCounter.java

---
+++ src/com/Collection/FrequencyCounter.java
@@ -0,0 +1,44 @@
+package com.Collection;
+
+import java.util.*;
+
+public class FrequencyCounter {
+    public static void main(String[] args) {
+        ArrayList<String> list=new ArrayList<>();
+        HashMap<String,Integer> hashMap=new HashMap<>();
+        list.add("Hello");
+        list.add("Luffy");
+        list.add("Hello");
+        list.add("Zoro");
+        for(String str:list){
+            hashMap.put(str,hashMap.getOrDefault(str,0)+1);
+
+        }
+
+
+        // for stream of String
+        String para="The series focuses on Monkey D. Luffy—a young man made of rubber after unintentionally eating the Gum-Gum Fruit—who sets off on a" +
+                " journey from the East Blue Sea to find the deceased King of the Pirates Gol D. Roger's ultimate treasure known as the \"One Piece\", and " +
+                "take over his prior title. Luffy sets sail as captain of the Straw Hat Pirates,[Jp 15] and is joined by Roronoa Zoro, a swordsman and former" +
+                " bounty hunter; Nami, a money-obsessed thief and navigator; Usopp, a sniper and compulsive liar; and Sanji, an amorous but chivalrous cook. They acquire a ship," +
+                " the Going Merry[Jp 16]—later replaced by the Thousand Sunny[Jp 17]—and engage in confrontations with notorious pirates. As Luffy and his crew set out on their" +
+                " adventures, others join the crew later in the series, including Tony Tony Chopper, an anthropomorphized reindeer doctor; Nico Robin, an archaeologist and former" +
+                " Baroque Works assassin; Franky, a cyborg shipwright; Brook, a skeleton musician and swordsman; and Jimbei, a whale shark-type fish-man and former member of the Seven " +
+                "Warlords of the Sea who becomes their helmsman. Together, they encounter other pirates, bounty hunters, criminal organizations, revolutionaries, secret agents, scientists, " +
+                "soldiers of the morally ambiguous World Government, and various other friends and foes, as they sail the seas in pursuit of their dreams.";
+        ArrayList<String> strList=new ArrayList<>(Arrays.asList(para.split(" ")));
+        HashMap<String,Integer> hashMap1=new HashMap<>();
+        for (String str:strList){
+            hashMap1.put(str, hashMap1.getOrDefault(str,0)+1);
+        }
+        for (String str:hashMap.keySet()){
+            System.out.println(str+" : "+ hashMap.get(str));
+        }
+        for (String str:hashMap1.keySet()){
+            System.out.println(str+" : "+ hashMap1.get(str));
+
+        }
+
+
+    }
+}
