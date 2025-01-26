82a6a844ed7510d8134f4fa7a81626c4245a2d1c
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-26 19:46:42 +0530

    concurrentHashMap demo

--- src/com/Collection/ConcurrentHashMapDemo.java
+++ src/com/Collection/ConcurrentHashMapDemo.java
@@ -1,5 +1,7 @@
 package com.Collection;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -7,58 +9,74 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConcurrentHashMapDemo {
     public static void main(String[] args) throws InterruptedException {
 
-        Map<Integer, String> map = new ConcurrentHashMap<>();
+        ArrayList<String> num=new ArrayList<>(Arrays.asList("one","two","three","four","five","six","seven","eight","nine",
+                "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty"));
 
+        Map<Integer, String> map = new ConcurrentHashMap<>(16,0.75f);
+        map.put(1,"one");
+        map.put(2,"two");
+        map.put(3,"thre");
+        map.put(4,"four");
+        map.put(5,"five");
+        map.put(6,"six");
+        map.put(7,"seveen");
+        map.put(8,"eight");
+        map.put(9,"nine");
+        map.put(10,"ten");
+        map.put(11,"eleven");
+        map.put(12,"twelve");
+        map.put(13,"thirtee");
+        map.put(14,"fourteen");
+        map.put(15,"fifteen");
+        map.put(16,"sixteen");
+        map.put(17,"seventeen");
+        map.put(18,"eighhteen");
+        map.put(19,"nineteen");
+        map.put(20,"twenty");
 
-        Thread t1 = new Thread(() -> {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        Thread update=new Thread(()->{
+            System.out.println("updating the thread b/w 1 to 10");
+            for(int i=0;i<10;i++){
+                System.out.println("thread 1");
+               if(!map.get(i+1).equals(num.get(i))){
+                  map.put(i,num.get(i));
+               }
             }
-            System.out.println("Thread t1 adding key 10");
-            map.putIfAbsent(10, "ten");
+            System.out.println("update thread has completed its work");
         });
-        Thread t2 = new Thread(() -> {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        Thread update2=new Thread(()->{
+            System.out.println("updating the thread b/w 11 to 20");
+            for(int i=11;i<20;i++){
+                System.out.println("thread 2");
+                if(!map.get(i).equals(num.get(i))){
+                    map.put(i,num.get(i));
+                }
             }
-            System.out.println("Thread t2 replacing key 1 -> One");
-            map.put(1, "One");
+            System.out.println("update2 thread has completed its work");
         });
-        Thread t3 = new Thread(() -> {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        Thread read=new Thread(()->{
+            System.out.println("read thread");
+            for(Map.Entry entry:map.entrySet()){
+                System.out.println("thread 3");
+                System.out.println("key : "+entry.getKey()+", value : "+entry.getValue());
             }
-            System.out.println("Thread t3 replacing key 1 -> Onee");
-            map.put(1, "Onee");
+            System.out.println("read thread completed");
         });
+        update.start();
+        System.out.println("main thread is running");
+        update2.start();
+        read.start();
+
+        update.join();
+        update2.join();
+        read.join();
+        System.out.println("all thread hae completed their work");
+        System.out.println(map);
 
 
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
 
 
-        t1.start();
-t2.start();
-t3.start();
-        for (Integer key : map.keySet()) {
-            System.out.println("Main thread iteration: " + key + " -> " + map.get(key));
-            Thread.sleep(1000); // Simulate work
-        }
 
 
-        t1.join();
-        t3.join();
-        t2.join();
-
-
-        System.out.println("Final map content:");
-        map.forEach((key, value) -> System.out.println(key + " -> " + value));
     }
 }
