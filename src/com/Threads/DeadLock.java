a0ddcbaa3a7bb4bbb6b574ae92402006a4d477e8
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-30 22:23:07 +0530

    DeadLock Implementation

---
+++ src/com/Threads/DeadLock.java
@@ -0,0 +1,73 @@
+package com.Threads;
+
+public class DeadLock {
+    public static void main(String[] args) {
+        Paper paper =new Paper();
+        Pen pen=new Pen();
+        Thread t1 = new Thread(new Task1(paper,pen));
+        Thread t2 = new Thread(new Task2(paper,pen));
+        t1.start();
+        t2.start();
+        try{
+            t1.join();
+            t2.join();
+        }catch(InterruptedException e){
+            System.out.println("hello");
+        }
+        System.out.println("main Thread");
+    }
+}
+class Pen{
+    public synchronized void writeWithPenAndPaper(Paper paper){
+
+            System.out.println(Thread.currentThread().getName()+"writing with pen and paper");
+            paper.finishWriting();
+
+
+    }
+    public synchronized void finishWriting(){
+        System.out.println("finished Writing");
+    }
+}
+class Paper{
+    public synchronized void writeWithPenAndPaper(Pen pen){
+
+            System.out.println(Thread.currentThread().getName()+"writing with pen and paper");
+            pen.finishWriting();
+
+
+    }
+    public synchronized void finishWriting(){
+        System.out.println("finished Writing");
+    }
+}
+class Task1 implements Runnable{
+    private Paper paper;
+    private Pen pen;
+
+    public Task1(Paper paper, Pen pen) {
+        this.paper = paper;
+        this.pen = pen;
+    }
+
+
+    @Override
+    public void run() {
+        paper.writeWithPenAndPaper(pen);
+    }
+}
+class Task2 implements Runnable{
+    private Paper paper;
+    private Pen pen;
+
+    public Task2(Paper paper, Pen pen) {
+        this.paper = paper;
+        this.pen = pen;
+    }
+
+
+    @Override
+    public void run() {
+        pen.writeWithPenAndPaper(paper);
+    }
+}
