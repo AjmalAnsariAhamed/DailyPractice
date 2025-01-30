a461f517c8b42d331ed23ff1a5e216ce3f5d736e
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-30 12:04:24 +0530

    deadLock with Locks

--- src/com/Threads/ThreadDeadLock.java
+++ src/com/Threads/ThreadDeadLock.java
@@ -1,39 +1,41 @@
 package com.Threads;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ThreadDeadLock {
-    private final Object lock1 = new Object();
-    private final Object lock2 = new Object();
+    private final Lock lock1 = new ReentrantLock();
+    private final Lock lock2 = new ReentrantLock();
 
     void OuterMethod() {
+        lock1.lock();
+        System.out.println(Thread.currentThread().getName() + " acquired lock1, waiting for lock2");
 
-        synchronized (lock1) {
-            System.out.println(Thread.currentThread().getName() + " acquired lock1, waiting for lock2");
-
-            try {
-                Thread.sleep(50); // Simulating some work
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-
-            synchronized (lock2) {
-                System.out.println(Thread.currentThread().getName() + " acquired lock2");
-            }
+        try {
+            Thread.sleep(50); // Simulate some work
+            lock2.lock();
+            System.out.println(Thread.currentThread().getName() + " acquired lock2");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            lock2.unlock();
+            lock1.unlock();
         }
     }
 
     void innerMethod() {
-        synchronized (lock2) {
-            System.out.println(Thread.currentThread().getName() + " acquired lock2, waiting for lock1");
+        lock2.lock();
+        System.out.println(Thread.currentThread().getName() + " acquired lock2, waiting for lock1");
 
-            try {
-                Thread.sleep(50); // Simulating some work
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-
-            synchronized (lock1) {
-                System.out.println(Thread.currentThread().getName() + " acquired lock1");
-            }
+        try {
+            Thread.sleep(50); // Simulate some work
+            lock1.lock();
+            System.out.println(Thread.currentThread().getName() + " acquired lock1");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            lock1.unlock();
+            lock2.unlock();
         }
     }
 
