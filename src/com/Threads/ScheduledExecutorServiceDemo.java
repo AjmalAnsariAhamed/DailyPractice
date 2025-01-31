24291d7066c3d3d999ed61383b8e7205adf19a33
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-31 22:50:40 +0530

    service.scheduleAtFixedRate()

---
+++ src/com/Threads/ScheduledExecutorServiceDemo.java
@@ -0,0 +1,32 @@
+package com.Threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorServiceDemo {
+    public static void main(String[] args) {
+        ScheduledExecutorService service= Executors.newScheduledThreadPool(2);
+        service.scheduleAtFixedRate(()->{
+
+            System.out.println("task execution after 5 minutes");
+
+        },0,4, TimeUnit.MILLISECONDS );
+        service.scheduleAtFixedRate(()->{
+
+            System.out.println("2nd task execution after 5 minutes");
+            try {
+                Thread.sleep(3);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            };
+        },0,4, TimeUnit.NANOSECONDS );
+// for stopping shutdown |
+        //               V
+        service.schedule(()->service.shutdown(),5,TimeUnit.NANOSECONDS);
+
+
+
+    }
+}
