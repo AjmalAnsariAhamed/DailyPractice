ead156962ed932212018ac8f26cbc25d6b067537
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-01-31 11:19:59 +0530

    ThreadPool with Executor Service

--- src/com/Threads/ThreadPool.java
+++ src/com/Threads/ThreadPool.java
@@ -1,5 +1,9 @@
 package com.Threads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadPool {
     public static int factorial(int num){
         if (num == 0 || num ==1) return 1;
@@ -13,15 +17,23 @@ public class ThreadPool {
     }
 
     public static void main(String[] args) {
-        Thread pool[]=new Thread[10];
-        for (int i=1;i<=10;i++){
+        ExecutorService executor= Executors.newFixedThreadPool(2);
+        for(int i=1;i<=10;i++){
             final int v=i;
-            pool[i-1]=new Thread(()->{
-
+            executor.submit(()->{
                 System.out.println(factorial(v));
             });
-            pool[i-1].start();
+        }
+        executor.shutdown();
+        try{
+            while(!executor.awaitTermination(10, TimeUnit.MILLISECONDS)){
+                System.out.println("running ");
+            }
+        }catch(InterruptedException e){
 
         }
+        System.out.println("all threads are terminated");
+
+
     }
 }
