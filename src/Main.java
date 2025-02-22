6b1911a75d3d6af2cddad149ac5a975739a53dbd
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-02-22 11:46:59 +0530

    reflectors implementation

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import com.OOPs.Abstract.*;
 import com.OOPs.Interfaces.*;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-       List<Animal> animal=new ArrayList<>();
-       animal.add(new Dog());
-       animal.add(new Cat());
-      for(Animal a:animal){
+    private String access="Acessed";
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Field field=Main.class.getDeclaredField("access");
+        field.setAccessible(true);
+        System.out.println( field.get(new Main())+" and "+field.get(new Main()).getClass());
+        //accessing the private fields
 
-          a.bark();
-      }
 
     }
 }
